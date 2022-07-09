4515d002717d72f8b8e51823d582c3de232be1ab
Akhil <akhilpdominic2001@gmail.com>
2022-07-09 18:58:48 +0530

    commited

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatefulWidget {
+  const homeScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<homeScreen> createState() => _homeScreen();
+}
+
+class _homeScreen extends State<homeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.blue[100],
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 150,
+              ),
+
+              Container(
+                margin: EdgeInsets.only(
+                  top: 100
+                ),
+                child: OutlinedButton(           
+                  style: OutlinedButton.styleFrom(
+                  primary: Colors.black,
+                  backgroundColor: Colors.blue[900],
+                  minimumSize: Size(300,55),
+                  textStyle: const TextStyle(fontSize: 20,color: Colors.black),
+                  ),
+                  onPressed: null,
+                  child: Text("Login",style: 
+                  TextStyle(color: Colors.white),)),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 25
+                ),
+                child: OutlinedButton(           
+                  style: OutlinedButton.styleFrom(
+                  primary: Colors.black,
+                  backgroundColor: Colors.orange[900],
+                  minimumSize: Size(300,55),
+                  textStyle: const TextStyle(fontSize: 20,color: Colors.black),
+                  ),
+                  onPressed: ()
+                {
+                  
+
+                }, 
+                  child: Text("Register",style: 
+                  TextStyle(color: Colors.white),)),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/startingPage.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class startingPage extends StatefulWidget {
+  const startingPage({ Key? key }) : super(key: key);
+
+  @override
+  State<startingPage> createState() => _startingPageState();
+}
+
+class _startingPageState extends State<startingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: 
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 20,
+              right: 20
+
+            ),
+            child: Container(
+              
+              child:TextField(decoration: 
+              InputDecoration(hintText: "startup,ideas",
+              border: OutlineInputBorder()),)
+            ),
+
+            
+          ),
+
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20
+            ),
+            height: 160,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.black26,
+              border: Border.all(
+                  color: Colors.black54,
+
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
+            child: Container
+            (
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 9,
+                  ),
+                  Text('Its a good day to start investing               ',
+                  style: TextStyle(fontSize: 15),
+                  ),
+
+                  Text('Make your first investment                ',
+                  style: TextStyle(fontSize: 17),
+                  ),
+
+                  Row(
+                    children: [
+                      SizedBox(width: 20),
+                      ElevatedButton(
+                        
+                        style: ElevatedButton.styleFrom(
+                          
+                        padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 4),
+                        shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                        child: const Text(
+      
+                          'Start here',
+                        style: TextStyle(fontSize: 12, 
+                        color: Colors.black),
+                        ),
+                        onPressed: null,
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      )
+      ),
+      
+    );
+  }
+}
